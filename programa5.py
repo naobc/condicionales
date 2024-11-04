f36318304aecbb44f7007f76ca86014fb285b240
Nao <naomibec123@gmail.com>
2024-11-03 20:28:31 -0600

    Programa 5

---
+++ programa5.py
@@ -0,0 +1,9 @@
+# Pedir nombre de usuario y contraseña al usuario
+usuario = input("Ingresa tu nombre de usuario: ")
+contraseña = input("Ingresa tu contraseña: ")
+
+# Verificar si las credenciales son correctas
+if usuario == "nayomis" and contraseña == "nayooos123":
+    print("Has entrado al sistema")
+else:
+    print("Error: Usuario o contraseña incorrectos")
