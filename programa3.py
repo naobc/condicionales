02e4e64677f6835ca6645b9ce428dc0f50b58f14
Nao <naomibec123@gmail.com>
2024-11-03 20:20:33 -0600

    Programa 3

---
+++ programa3.py
@@ -0,0 +1,8 @@
+# Pedir un número al usuario
+num = int(input("Ingresa un número: "))
+
+# Verificar si el número es par o impar
+if num % 2 == 0:
+    print("El número es par")
+else:
+    print("El número es impar")
